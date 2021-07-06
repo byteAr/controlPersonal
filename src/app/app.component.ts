7f2c8cec9c96d373967ed1c602f0709543f5de57
Marcos Lopez <marcosmatiaslopez.2018@gmail.com>
2021-07-06 07:49:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
+
 import { Component } from '@angular/core';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService]
 })
 export class AppComponent {
-  title = 'guardia';
+
+  dni=''
+
+  constructor(private messageService: MessageService) {}
+
+  auth() {
+    if(this.dni === '') {
+      this.messageService.add({severity:'error', summary:'', detail:'Debe ingresar un número de DNI'});
+      return
+    }
+    console.log(this.dni);
+    this.messageService.add({severity:'success', summary:'', detail:'Esta persona se encuentra autorizada a ingresar'});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//PrimeNg
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {ToastModule} from 'primeng/toast';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +21,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    CardModule,
+    InputTextModule,
+    ToastModule,
+    FormsModule,
+    CommonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
